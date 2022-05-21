787e65f39ad33da160f4263ad7d0c0bb8d79db2d
Kleiton MQ <kleiton.x5@gmail.com>
2022-05-20 22:46:52 -0300

    conclusao das aulas de filesystemwatcher.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿static void IniciarAplicacao()
+﻿using static System.Console;
+
+static void IniciarAplicacao()
 {
     CriarDiretorios();
     CriarArquivo();
@@ -117,7 +119,36 @@ foreach (var arquivo in arquivos)
 }
 }
 
-var path = @"E:\Projetosdev\DirectoryAndDirectoryInfo\";
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+    WriteLine($"Foi criado o arquivo {e.Name}");
+}
+
+void OnRenamed(object sender, RenamedEventArgs e)
+{
+    WriteLine($"Foi renomeado de {e.OldName} para {e.Name}");
+}
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+    WriteLine($"Arquivo Deletado {e.Name}");
+}
+
+
+var path = @"C:\Users\cce-teste37\Documents\Bootcamp\";
 //LerDiretorios(path);
-LerArquivos(path);
+//LerArquivos(path);
+
+
+using var fsw = new FileSystemWatcher(path);
+
+fsw.Created += OnCreated;
+fsw.Renamed += OnRenamed;
+fsw.Deleted += OnDeleted;
+
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+WriteLine("Pressione enter  para sair");
 Console.ReadLine();
